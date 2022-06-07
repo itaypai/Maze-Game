ece0e15c916b93316f29630f3f43d9a0062ccbe3
itayp <itaypai@post.bgu.ac.il>
2022-06-07 19:39:04 +0300

    Implementation of SimpleCompressorOutputStream and SimpleDecompressorInputStream

--- ATP-Project-PartB/src/IO/SimpleCompressorOutputStream.java
+++ ATP-Project-PartB/src/IO/SimpleCompressorOutputStream.java
@@ -1,4 +1,71 @@
 package IO;
 
-public class SimpleCompressorOutputStream {
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * SimpleCompressorOutputStream class extends OutputStream and implements a compressor by counting the number of
+ * sequences of unity and zeros in the maze.
+ */
+public class SimpleCompressorOutputStream extends OutputStream {
+    OutputStream out;
+
+    /**
+     * Constructor
+     * @param out
+     */
+    public SimpleCompressorOutputStream(OutputStream out)
+    {
+        this.out = out;
+    }
+
+    /**
+     * @param b
+     * @throws IOException
+     */
+    @Override
+    public void write(int b) throws IOException {
+        out.write(b);
+    }
+
+    /**
+     * An array of bytes containing all the details of the maze. The details of the maze are compressed
+     * according to the method of counting the 0's and 1's in the maze.
+     * All the details of the maze are compressed and sent through the output stream.
+     * @param b, an array of bytes containing all the details of the maze.
+     * @throws IOException
+     */
+    @Override
+    public void write(byte[] b) throws IOException {
+        int sequenceCount = 0;
+        for (int i=0; i < b.length; i++)
+        {
+            if (i < 12){
+                this.write(b[i]);
+            }
+            else{
+                if (i == 12){
+                    if (b[i] == 1) {
+                        this.write(0);
+                    }
+                    sequenceCount++;
+                }
+                else {
+                    if (b[i] == b[i - 1]){
+                        sequenceCount++;
+                    }
+                    else{
+                        while (sequenceCount > 255)
+                        {
+                            this.write(255);
+                            this.write(0);
+                            sequenceCount = sequenceCount - 255;
+                        }
+                        this.write(sequenceCount);
+                        sequenceCount = 1;
+                    }
+                }
+            }
+        }
+    }
 }
--- ATP-Project-PartB/src/IO/SimpleDecompressorInputStream.java
+++ ATP-Project-PartB/src/IO/SimpleDecompressorInputStream.java
@@ -1,4 +1,53 @@
 package IO;
 
-public class SimpleDecompressorInputStream {
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SimpleDecompressorInputStream extends InputStream {
+    InputStream in;
+
+    public SimpleDecompressorInputStream(InputStream in)
+    {
+        this.in = in;
+    }
+
+    /**
+     * @return
+     * @throws IOException
+     */
+    @Override
+    public int read() throws IOException {
+        return in.read();
+    }
+
+    /**
+     * Extract the information of the input through the input stream into array b according
+     * to the method of compression.
+     * @param b, we "inflate" the array with the extracted information.
+     * @return
+     * @throws IOException
+     */
+    @Override
+    public int read(byte[] b) throws IOException {
+        int currNumber = 0;
+        int extractedIndex = 0;
+        int currInput = this.in.read();
+        while (currInput != -1)
+        {
+            if (extractedIndex < 12){
+                b[extractedIndex] = (byte)currInput;
+                extractedIndex++;
+            }
+            else {
+                for (int j=0; j < currInput; j++)
+                {
+                    b[extractedIndex] = (byte)currNumber;
+                    extractedIndex++;
+                }
+                currNumber = 1 - currNumber;
+            }
+            currInput = this.in.read();
+        }
+        return extractedIndex;
+    }
 }
